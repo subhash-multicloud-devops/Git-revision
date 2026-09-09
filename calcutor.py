9cce4e1553f0fef53fca1fb5b782582396949fe9
subhash-multicloud-devops <subhashregati2@gmail.com>
2026-09-10 00:29:41 +0530

    updated caclcultor-2

--- calcutor.py
+++ calcutor.py
@@ -4,6 +4,9 @@ def add(a: float, b: float) -> float:
 def subtract(a: float, b: float) -> float:
     return a - b
 
+def multiply(a: float, b: float) -> float:
+    return a * b
+
 def divide(a: float, b: float) -> float:
     if b == 0:
         raise ZeroDivisionError("Cannot divide by zero.")
